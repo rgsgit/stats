541281e6b58feb82194453e836c438a5b9343a31
Ravshan Sharifov <sharifovrav@gmail.com>
2021-04-06 17:46:39 +0500

    fix

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -7,20 +7,21 @@ import (
 //Avg рассчитывает среднюю сумму платежа.
 func Avg(payments []types.Payment) types.Money {
 	sum := types.Money(0)
+	cnt := types.Money(0)
 	for _, payment := range payments {
-		if payment.Status != types.StatusFail{
-		sum += payment.Amount
+		if payment.Status != types.StatusFail {
+			sum += payment.Amount
+			cnt++
 		}
 	}
-	sum = types.Money((int(sum)) / len(payments))
-	return sum
+	return sum / cnt
 }
 
 //TotalInCategory находит сумму покупок в определённой категории
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	sum := types.Money(0)
 	for _, payment := range payments {
-		if payment.Category == category && payment.Status != types.StatusFail{
+		if payment.Category == category && payment.Status != types.StatusFail {
 			sum += payment.Amount
 		}
 	}
